931c2648e61901fe4a93416007547d63b3b665d7
Jackie Santana <jackietristansantana@gmail.com>
2020-11-09 00:31:32 -0500

    database hooked up to mongodb

--- model/database.js
+++ model/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/SantanaLaF', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 const Form = mongoose.model('Form', 
 { 
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
 const app = express()
-const path = require('path')
+
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config({ path: "./config/.env" })
 
 PORT = process.env.PORT
-// const viewsPath = path.join(__dirname, './templates/views')
-// const partialsPath = path.join(__dirname, './templates/partials')
+
 
 //handlebars middleware 9have to initialize this in order to use handlebars
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.engine('handlebars', exphbs( {defaultLayout: 'main'} ));
 app.set('view engine', 'handlebars')
